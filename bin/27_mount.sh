74196258963cf43408b0342ca2c7af4dae46b5b1
Chertolias Andrei <andrey.chertolyas@gmail.com>
2021-10-30 23:55:02 +0400

    Enable swap

--- bin/27_mount.sh
+++ bin/27_mount.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 sudo mkdir -pv $LFS
 sudo mount -v -t ext4 /dev/sdb3 $LFS
-sudo mkdir -v $LFS/swap
-sudo mount -v -t swap /dev/sdb1 $LFS/swap
 sudo mkdir -v $LFS/boot
 sudo mount -v -t ext4 /dev/sdb2 $LFS/boot
 sudo mkdir -v $LFS/home
 sudo mount -v -t ext4 /dev/sdb4 $LFS/home
+
+echo "Enabling swap..."
+sudo /sbin/swapon -v /dev/sdb1
