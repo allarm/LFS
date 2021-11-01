5add3ba197f2f13d2cac70e7d92cb3b07a16702d
Chertolias Andrei <andrey.chertolyas@gmail.com>
2021-11-01 22:37:26 +0400

    Fixed substitutions in heredoc

--- bin/44_setup_environment.sh
+++ bin/44_setup_environment.sh
@@ -5,14 +5,20 @@ cat > /home/"${LFS_USER}"/.bash_profile << "EOF"
 exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 EOF
 
-cat > /home/"${LFS_USER}"/.bashrc << "EOF"
+cat > /home/"${LFS_USER}"/.bashrc << EOF
 set +h
 umask 022
-LFS=${LFS}
+LFS=$LFS
 LC_ALL=POSIX
+EOF
+
+cat >> /home/"${LFS_USER}"/.bashrc << "EOF"
 LFS_TGT=$(uname -m)-lfs-linux-gnu
 PATH=/usr/bin
 if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
+EOF
+
+cat >> /home/"${LFS_USER}"/.bashrc << EOF
 PATH=$LFS/tools/bin:$PATH
 CONFIG_SITE=$LFS/usr/share/config.site
 export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
